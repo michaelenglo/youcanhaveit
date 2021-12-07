a417bff182e055ac800b9f2f7f014e44a508cc48
Michael Englo <micahelenglo@gmail.com>
2021-12-06 21:39:21 -0800

    add basic routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WishItemListPageComponent } from './wish-item-list-page/wish-item-list-page.component';
 
 
+const routes: Routes = [
+  { path: 'wish-items', component: WishItemListPageComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
