a170fd64db2fa106e3c3d7b81822fcd0ecde5723
Michael Englo <micahelenglo@gmail.com>
2021-12-06 21:28:06 -0800

    add routing module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WishItemListPageComponent } from './wish-item-list-page/wish-item-list-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { WishItemListPageComponent } from './wish-item-list-page/wish-item-list-
     WishItemListPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
